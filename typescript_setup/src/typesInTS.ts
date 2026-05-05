8027a0a562ac4120a79784b67e00ec64fe2e1c66
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-05-06 00:00:12 +0530

    new

---
+++ typescript_setup/src/typesInTS.ts
@@ -0,0 +1,19 @@
+//TypeScript enhances JavaScript by adding static types.
+//Type Annotations and Inference
+// TypeScript offers two ways to work with types:
+
+// Explicit Typing: You explicitly declare the type of a variable
+// Type Inference: TypeScript automatically determines the type based on the assigned value
+
+//Type Inference:
+let drink = "chai";
+let cups = Math.random() > 0.5 ? 10:5;
+
+//Type Annotation: Explicit typing means you tell TypeScript exactly what type a variable should be
+let chaiFlaovour: String = "masala chai"  //string
+chaiFlaovour = "Ginger Tea"
+
+let chaiOrder:number   //number
+
+let isActive: boolean = true;
+let hasPermission = false;     //boolean
